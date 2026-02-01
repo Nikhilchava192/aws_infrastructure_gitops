cbfefe684900c7e1d7d224680d8309ce11ffb9e6
Nikhil <nikhilchava2003@gmail.com>
2026-02-01 14:38:23 +0530

    ok

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+    features {
+     
+    }
+    subscription_id = "0e313e54-8a07-4b89-b511-7c70ab1b59b2"
+    resource_provider_registrations = "none"
+}
---
+++ resource.tf
@@ -0,0 +1,134 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "AZLB"
+  location = "Central India"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+resource "azurerm_public_ip" "lb_pip" {
+  name                = "pip-lb-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+resource "azurerm_lb" "lb" {
+  name                = "lb-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "frontend-ip"
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
+  }
+}
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  name            = "backend-pool"
+  loadbalancer_id = azurerm_lb.lb.id
+}
+resource "azurerm_lb_probe" "http_probe" {
+  name            = "http-probe"
+  loadbalancer_id = azurerm_lb.lb.id
+  protocol        = "Http"
+  port            = 80
+  request_path    = "/"
+}
+resource "azurerm_lb_rule" "http_rule" {
+  name                           = "http-rule"
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "frontend-ip"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend_pool.id]
+  probe_id                       = azurerm_lb_probe.http_probe.id
+}
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "nic-vm-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_network_interface_backend_address_pool_association" "nic_lb_assoc" {
+  count                   = 2
+  network_interface_id    = azurerm_network_interface.nic[count.index].id
+  ip_configuration_name   = "ipconfig1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = 2
+  name                = "vm-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_D2s_v3"
+  disable_password_authentication = false
+  admin_username      = "nikhil"
+  admin_password      = "Nikhil@200319"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
